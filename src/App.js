57f8fa02433f0ac22afc14e622955ee9dd595dc6
tjs6595 <t.schultejans@schenckprocess.com>
2022-07-12 21:28:24 -0500

    Redo of Music Library back to before Context work.

--- src/App.js
+++ src/App.js
@@ -7,25 +7,36 @@ function App(){
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
 
+    const API_URL = 'https://itunes.apple.com/search?term='
+
     useEffect(() => {
+      if(search) {
         const fetchData = async () => {
             document.title = `${search} Music`
-            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+            const response = await fetch(API_URL + search)
             const resData = await response.json()
-            console.log(resData)
+            if (resData.results.length > 0) {
+              setData(resData.results)
+          } else {
+              setMessage('Not Found')
+          }
         }
-        fetchData()
-    })
+      fetchData()
+      }
+    }, [search])
+
+    const handleSearch = (e, term) => {
+      e.preventDefault()
+      setSearch(term)
+    }
 
     return (
         <div>
-            <SearchBar />
+            <SearchBar handleSearch = {handleSearch} />
             {message}
-            <Gallery />
+            <Gallery data={data}/>
         </div>
     )
 }
 
 export default App
-
-
---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
+        return (
+            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here"
+            onChange={(e) => props.handleSearch(e, e.target.value)} />
+
+            <input type="submit" />
+
+            </form>
+    )
+}
+
+export default SearchBar
+
